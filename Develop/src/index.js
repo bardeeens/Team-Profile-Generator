295adcce62defa66f61133b0ac0933a28a1383f2
Bardini <derekbardini@gmail.com>
2021-01-30 19:21:44 -0600

    questions displaying properly

--- Develop/src/index.js
+++ Develop/src/index.js
@@ -4,6 +4,7 @@ const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
 const Manager = require("../lib/Manager");
 const generate = require("./page-template")
+let staffArray = []
 
 // const questions = require('Develop\src\questions.js');
 const { managerQuestions, internQuestions,
@@ -33,10 +34,39 @@ let manager = () => {
         .prompt(managerQuestions)
         .then((response) => {
         let manager = new Manager(response.name, response.id, response.email, response.officeNumber);
-        
+        staffArray.push(manager)
+        if (response.moreEmployees){
+            employeeType();
+        }
 
     })
-}
+};
+
+let intern = () => {
+    inquirer
+        .prompt(internQuestions)
+        .then((response) => {
+        let intern = new Intern(response.name, response.id, response.email, response.school);
+        staffArray.push(intern)
+        if (response.moreEmployees){
+            employeeType();
+        }
+
+    })
+};
+
+let engineer = () => {
+    inquirer
+        .prompt(engineerQuestions)
+        .then((response) => {
+        let engineer = new Intern(response.name, response.id, response.email, response.github);
+        staffArray.push(engineer)
+        if (response.moreEmployees){
+            employeeType();
+        }
+
+    })
+};
 
 employeeType();
     
